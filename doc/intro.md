81ac356b71adfbc9086787ff7847a5dcce19e0c6
Martín Varela <martin.varela@profilence.com>
2023-06-07 10:13:21 +0300

    let there be light

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj.intro/dotslash
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj/intro/dotslash_test.clj
@@ -0,0 +1,7 @@
+(ns clj.intro.dotslash-test
+  (:require [clojure.test :refer :all]
+            [clj.intro.dotslash :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
